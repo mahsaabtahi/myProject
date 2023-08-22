4605bc53820dafc0121ed89a77dfa6b5a5ef4ad9
mahsa abtahi <mahsajaniii13@gmail.com>
2023-08-22 23:33:22 +0330

    creat function to show result but it dosent work correctly

--- calculator.js
+++ calculator.js
@@ -1,21 +1,41 @@
 'use strict';
 const input=document.getElementById('cal__input');
-const plus=document.querySelector('.plus');
-const minus=document.querySelector('.minus');
-const mul=document.querySelector('.multiplication');
-const clear=document.querySelector('.clear');
-const result=document.querySelector('.result');
-const divide=document.querySelector('.divide');
+
+let currentNum='';
+let currentOp='';
+
 function displayNumber(number){
-    input.value += number;
+    currentNum = number;
+    input.value += currentNum;
 }
 function displayOpration(operand){
+    currentOp=operand;
+    currentNum='';
+    input.value="";
 
 }
 function clearNumber(){
     input.value="";
+    currentNum='';
+    currentOp='';
 }
 function calculatResult(){
-    
+    let num1=parseFloat(input.value);
+    let num2=parseFloat(currentNum);
+    switch(currentOp){
+        case '+':
+            input.value=num1+num2;
+            break;
+        case '-':
+            input.value=num1-num2;
+            break;
+        case '*':
+            input.value=num1*num2;
+            break;
+        case '/':
+            input.value=num1/num2;
+            break;
+    }
+
 }
 
