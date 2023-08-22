fba666df8b8a1d50b36dcd8de4b02edc0e38368b
mahsa abtahi <mahsajaniii13@gmail.com>
2023-08-22 21:42:54 +0330

    creat function in js to display number

---
+++ calculator.js
@@ -0,0 +1,21 @@
+'use strict';
+const input=document.getElementById('cal__input');
+const plus=document.querySelector('.plus');
+const minus=document.querySelector('.minus');
+const mul=document.querySelector('.multiplication');
+const clear=document.querySelector('.clear');
+const result=document.querySelector('.result');
+const divide=document.querySelector('.divide');
+function displayNumber(number){
+    input.value += number;
+}
+function displayOpration(operand){
+
+}
+function clearNumber(){
+    input.value="";
+}
+function calculatResult(){
+    
+}
+
